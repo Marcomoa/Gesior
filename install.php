762da4bea10e6aacdeef11fadeeb9b0f552c303d
Marco Oliveira <marcomoa@hotmail.com>
2020-06-04 22:13:44 -0300

    fix it

--- install.php
+++ install.php
@@ -29,26 +29,6 @@ spl_autoload_register('autoLoadClass');
 
 $config = array();
 include('./config/config.php');
-if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
-	$process = array(
-		&$_GET,
-		&$_POST,
-		&$_COOKIE,
-		&$_REQUEST
-	);
-
-	while (list($key, $val) = each($process)) {
-		foreach ($val as $k => $v) {
-			unset($process[$key][$k]);
-			if (is_array($v)) {
-				$process[$key][stripslashes($k)] = $v;
-				$process[] =& $process[$key][stripslashes($k)];
-			} else
-				$process[$key][stripslashes($k)] = stripslashes($v);
-		}
-	}
-	unset($process);
-}
 
 $page = '';
 if (isset($_REQUEST['page']))
