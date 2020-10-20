49578e349de826d442131a12fb1424db2b26de65
Marco Oliveira <marcomoa@hotmail.com>
2020-10-20 00:03:13 -0300

    Code cleanups and removing unused images (#20)

--- headline.php
+++ headline.php
@@ -1,5 +1,5 @@
 <?php
-//$text = $_GET['text'];
+
 require_once "custom_scripts/gd-text/Box.php";
 require_once "custom_scripts/gd-text/Color.php";
 require_once "custom_scripts/gd-text/HorizontalAlignment.php";
@@ -20,30 +20,17 @@ imagefill($im, 0, 0, imagecolortransparent($im, null));
 
 
 $box = new Box($im);
-$box->setFontFace("images/martel.ttf"); // http://www.dafont.com/elevant-by-pelash.font
+$box->setFontFace("images/fonts/martel.ttf");
 $box->setFontSize(25);
 $box->setFontColor(new Color(240, 209, 164));
 $box->setBox(0, -5, 500, 35);
 $box->setTextAlign('left', 'top');
 
-$box->setStrokeColor(new Color(1, 1, 1)); // Set stroke color
-$box->setStrokeSize(1); // Stroke size in pixels
+$box->setStrokeColor(new Color(1, 1, 1));
+$box->setStrokeSize(1);
 
-$box->draw($text); // Text to draw
+$box->draw($text);
 
 header("Content-type: image/png;");
 imagepng($im, null, 9, PNG_ALL_FILTERS);
 die();
-
-/** Just for save the original code
-if (!file_exists('images/head/' . $text . '.png')) {
-    $font = "images/martel.ttf";
-    $image = imagecreatefrompng('images/head/headline.png');
-    imagettftext($image, 18, 0, 4, 20, imagecolorallocate($image, 240, 209, 164), $font, $text);
-    imagepng($image, 'images/head/' . $text . '.png');
-}
-$img = 'images/head/' . $text . '.png';
-$pic = imagecreatefrompng($img);
-header('Content-type: image/png');
-imagepng($pic);
- * */
--- pages/headline.php
+++ pages/headline.php
@@ -19,39 +19,17 @@ imagefill($im, 0, 0, imagecolortransparent($im, null));
 
 
 $box = new Box($im);
-$box->setFontFace("../images/martel.ttf"); // http://www.dafont.com/elevant-by-pelash.font
+$box->setFontFace("../images/fonts/martel.ttf");
 $box->setFontSize(24);
 $box->setFontColor(new Color(240, 209, 164));
 $box->setBox(4, -4, 260, 28);
 $box->setTextAlign('left', 'top');
 
-$box->setStrokeColor(new Color(1, 1, 1)); // Set stroke color
-$box->setStrokeSize(0); // Stroke size in pixels
+$box->setStrokeColor(new Color(1, 1, 1));
+$box->setStrokeSize(0);
 
-$box->draw($text); // Text to draw
+$box->draw($text);
 
 header("Content-type: image/png;");
 imagepng($im, null, 0, PNG_ALL_FILTERS);
 die();
-
-/*
-	$text = $_GET['txt'];
-	$text = strtoupper($text[0]).substr($text,1,strlen($text));
-	$size = 18;
-	$sizex = 280;
-	$sizey = 28;
-	$x = 4;
-	$y = 20;
-	$color = 'efcfa4';
-		$red = (int)hexdec(substr($color,0,2));
-		$green = (int)hexdec(substr($color,2,2));
-		$blue = (int)hexdec(substr($color,4,2));
-	$img = imagecreatetruecolor($sizex,$sizey);
-	ImageColorTransparent($img, ImageColorAllocate($img,0,0,0));
-	
-	imagefttext($img, $size, 0, $x, $y, ImageColorAllocate($img,$red,$green,$blue), '../images/martel.ttf', $text);
-	
-	header('Content-type: image/png');
-	imagepng($img);
-	imagedestroy($img);
-*/
